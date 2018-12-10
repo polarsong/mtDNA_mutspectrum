3cf9ee697a4621b2f762456f6af1d270efc7afc1
Alya <polarsong4@gmail.com>
2018-12-10 19:06:06 +0200

    smth I dont know

--- Head/2Scripts/WholeGenomeAnalyses.EcologyAndMutSpecChordata.R
+++ Head/2Scripts/WholeGenomeAnalyses.EcologyAndMutSpecChordata.R
@@ -41,43 +41,47 @@ for (i in 1:length(VecOfTaxaShort))
   SynNucAA = merge(AA,SynNuc)
   length(unique(SynNucAA$Species))  # 192 species
 
-  ########### which nucleotides better correlate with GT
+  ########### question 1: which nucleotides better correlate with GT
   AGG = aggregate(list(SynNucAA$FrA,SynNucAA$FrT,SynNucAA$FrG,SynNucAA$FrC), by = list(SynNucAA$Species,SynNucAA$Female.maturity..days.), FUN = mean)
   names(AGG) = c('Species','FemaleMaturityDays','FrA','FrT','FrG','FrC')
 
+  ###### start from pairwise correlations and go to multiple linear model:
+  ## it is opposite s compared to mammals
   par(mfrow=c(2,2),oma = c(0, 0, 2, 0),cex.main = 2, cex.lab = 2)
-  plot(log2(AGG$FemaleMaturityDays),AGG$FrA, col = 'gray', ylim = c(0,1), xlab = 'log2(GT)', main = 'FrA', cex = 2); abline(h =0.4, lt = 2, col = 'red')
-  plot(log2(AGG$FemaleMaturityDays),AGG$FrT, col = 'blue', ylim = c(0,1), xlab = 'log2(GT)', main = 'FrT', cex = 2); abline(h =0.4, lt = 2, col = 'red')
-  plot(log2(AGG$FemaleMaturityDays),AGG$FrG, col = 'green', ylim = c(0,1), xlab = 'log2(GT)', main = 'FrG', cex = 2); abline(h =0.4, lt = 2, col = 'red')
-  plot(log2(AGG$FemaleMaturityDays),AGG$FrC, col = 'cyan', ylim = c(0,1), xlab = 'log2(GT)', main = 'FrC', cex = 2); abline(h =0.4, lt = 2, col = 'red')
+  plot(log2(AGG$FemaleMaturityDays),AGG$FrA, col = 'gray', ylim = c(0,1), xlab = 'log2(GT)', main = 'FrA'); abline(h =0.4, lt = 2, col = 'red')
+  plot(log2(AGG$FemaleMaturityDays),AGG$FrT, col = 'blue', ylim = c(0,1), xlab = 'log2(GT)', main = 'FrT'); abline(h =0.4, lt = 2, col = 'red')
+  plot(log2(AGG$FemaleMaturityDays),AGG$FrG, col = 'green', ylim = c(0,1), xlab = 'log2(GT)', main = 'FrG'); abline(h =0.4, lt = 2, col = 'red')
+  plot(log2(AGG$FemaleMaturityDays),AGG$FrC, col = 'cyan', ylim = c(0,1), xlab = 'log2(GT)', main = 'FrC'); abline(h =0.4, lt = 2, col = 'red')
   mtext(VecOfTaxaShort[i], outer = TRUE, cex = 1.5)
 
-  ########### which nucleotides better correlate with MLS
+  ########### question 1: which nucleotides better correlate with GT
   AGG = aggregate(list(SynNucAA$FrA,SynNucAA$FrT,SynNucAA$FrG,SynNucAA$FrC), by = list(SynNucAA$Species,SynNucAA$Maximum.longevity..yrs.), FUN = mean)
   names(AGG) = c('Species','MaxLifeSpan','FrA','FrT','FrG','FrC')
   
+  ###### start from pairwise correlations and go to multiple linear model:
+  ## it is opposite s compared to mammals
   par(mfrow=c(2,2),oma = c(0, 0, 2, 0),cex.main = 2, cex.lab = 2)
-  plot(log2(AGG$MaxLifeSpan),AGG$FrA, col = 'gray', ylim = c(0,1), xlab = 'log2(MLS)', main = 'FrA', cex = 2); abline(h =0.4, lt = 2, col = 'red')
-  plot(log2(AGG$MaxLifeSpan),AGG$FrT, col = 'blue', ylim = c(0,1), xlab = 'log2(MLS)', main = 'FrT', cex = 2); abline(h =0.4, lt = 2, col = 'red')
-  plot(log2(AGG$MaxLifeSpan),AGG$FrG, col = 'green', ylim = c(0,1), xlab = 'log2(MLS)', main = 'FrG', cex = 2); abline(h =0.4, lt = 2, col = 'red')
-  plot(log2(AGG$MaxLifeSpan),AGG$FrC, col = 'cyan', ylim = c(0,1), xlab = 'log2(MLS)', main = 'FrC', cex = 2); abline(h =0.4, lt = 2, col = 'red')
+  plot(log2(AGG$MaxLifeSpan),AGG$FrA, col = 'gray', ylim = c(0,1), xlab = 'log2(GT)', main = 'FrA'); abline(h =0.4, lt = 2, col = 'red')
+  plot(log2(AGG$MaxLifeSpan),AGG$FrT, col = 'blue', ylim = c(0,1), xlab = 'log2(GT)', main = 'FrT'); abline(h =0.4, lt = 2, col = 'red')
+  plot(log2(AGG$MaxLifeSpan),AGG$FrG, col = 'green', ylim = c(0,1), xlab = 'log2(GT)', main = 'FrG'); abline(h =0.4, lt = 2, col = 'red')
+  plot(log2(AGG$MaxLifeSpan),AGG$FrC, col = 'cyan', ylim = c(0,1), xlab = 'log2(GT)', main = 'FrC'); abline(h =0.4, lt = 2, col = 'red')
   mtext(VecOfTaxaShort[i], outer = TRUE, cex = 1.5)
   
-  ########### question 2: which genes better correlate with GT
+  
+  ########### question 2: which genes better correlate with GT (why T in ATP6,COX3 and ND4 do not correlate with GT and high absolute value - fast replication, no tRNA before them?)
   ## T is negatively and C is positively (T->C)
-  VecOfGenes = c('COX1','COX2','ATP8','ATP6','COX3','ND3','ND4L','ND4','ND5','ND6','CytB','ND1','ND2')
-  length(VecOfGenes)
-  par(mfcol=c(4,13),oma = c(0, 0, 3, 0),cex.main = 2, cex.lab = 2)
-  for (j in 1:length(VecOfGenes))
-    { # j = 1
-    OneGene = SynNucAA[SynNucAA$Gene == VecOfGenes[j],]
-    main = VecOfGenes[j]
-    plot(log2(OneGene$Female.maturity..days.),OneGene$FrA, col = 'gray', main = main, ylim = c(0,1), xlab = 'log2(MLS)') # a bit negative
-    plot(log2(OneGene$Female.maturity..days.),OneGene$FrT, col = 'blue', main = main, ylim = c(0,1), xlab = 'log2(MLS)') # a bit negative
-    plot(log2(OneGene$Female.maturity..days.),OneGene$FrG, col = 'green', main = main, ylim = c(0,0.6), xlab = 'log2(MLS)') # a bit negative
-    plot(log2(OneGene$Female.maturity..days.),OneGene$FrC, col = 'cyan', main = main, ylim = c(0,0.6), xlab = 'log2(MLS)') # a bit negative
-    }
-    mtext(VecOfTaxaShort[i], outer = TRUE, cex = 1.5)
+  #VecOfGenes = c('COX1','COX2','ATP8','ATP6','COX3','ND3','ND4L','ND4','ND5','ND6','CytB','ND1','ND2') # ND1 ND2 ND6
+  #length(VecOfGenes)
+  #par(mfcol=c(4,13))
+  #for (i in 1:length(VecOfGenes))
+  #{ # i = 1
+  #OneGene = SynNucAA[SynNucAA$Gene == VecOfGenes[i],]
+  #main = VecOfGenes[i]
+  #plot(log2(OneGene$Female.maturity..days.),OneGene$FrA, col = 'gray', main = main, ylim = c(0,1), xlab = 'log2(FM)') # a bit negative
+  #plot(log2(OneGene$Female.maturity..days.),OneGene$FrT, col = 'blue', main = main, ylim = c(0,1), xlab = 'log2(FM)') # a bit negative
+  #plot(log2(OneGene$Female.maturity..days.),OneGene$FrG, col = 'green', main = main, ylim = c(0,0.6), xlab = 'log2(FM)') # a bit negative
+  #plot(log2(OneGene$Female.maturity..days.),OneGene$FrC, col = 'cyan', main = main, ylim = c(0,0.6), xlab = 'log2(FM)') # a bit negative
+  #}
 }  
 dev.off()
 
