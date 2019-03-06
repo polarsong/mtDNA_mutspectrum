179840f9248f7e8c04c5a43f1cee6b96eae5d0c2
konstantinpopadin <konstantinpopadin@gmail.com>
2019-03-06 22:04:03 +0100

    only mammals

--- Head/2Scripts/VertebratePolymorphisms.MutSpecComparisons.Analyses.Ecology.Actinopterygii.R
+++ Head/2Scripts/VertebratePolymorphisms.MutSpecComparisons.Analyses.Ecology.Actinopterygii.R
@@ -1,9 +1,8 @@
 rm(list=ls(all=TRUE))
 
-
 MUT = read.table('../../Body/3Results/VertebratePolymorphisms.MutSpecData.OnlyFourFoldDegAllGenes.txt', header = TRUE)
 
-### longevity in fishes
+### longevity in fishes - nothing:
 
 AnAge = read.table('../../Body/1Raw/anage_data.txt', sep = '\t', header = TRUE)
 AnAge$Species = paste(AnAge$Genus,AnAge$Species,sep = '_')
@@ -23,43 +22,95 @@ cor.test(AnAgeMut$C_A,AnAgeMut$Maximum.longevity..yrs., method = 'spearman')
 cor.test(AnAgeMut$C_T,AnAgeMut$Maximum.longevity..yrs., method = 'spearman')
 cor.test(AnAgeMut$C_G,AnAgeMut$Maximum.longevity..yrs., method = 'spearman')
 
-### temperature in fishes: T_C is positive!?
+### temperature in fishes: 
 
-Temper = read.table('../../Body/1Raw/TemperatureAllChordataDataSet.txt', sep = '\t', header = TRUE)
-Temper = Temper[Temper$Tax == 'fishes',]
-Temper$Species = gsub(' ','_',Temper$Species)
-TemperMut = merge(MUT,Temper) # 41 just
+Temper = read.table('../../Body/1Raw/BodyParametersForAllClasses.txt',header = TRUE)
+table(Temper$Class)
+Temper = Temper[Temper$Class == 'Actinopterygii',]
+TemperMut = merge(MUT,Temper) # 320 species!!!
+TemperMut$T_C.NormalOnlyByT = TemperMut$T_C / (TemperMut$T_C  + TemperMut$T_A + TemperMut$T_G )
 
-cor.test(TemperMut$T_C,TemperMut$T..oC., method = 'spearman') # positive!!!! Why????
-plot(TemperMut$T_C,TemperMut$T..oC.)
-cor.test(TemperMut$T_A,TemperMut$T..oC., method = 'spearman')
-cor.test(TemperMut$T_G,TemperMut$T..oC., method = 'spearman')
+###### Temperature, Mass, MetabolRate
+###### Temperature ~ T_C 
+cor.test(TemperMut$T_C,TemperMut$Temperature, method = 'spearman') # super positive!!!! rho = 0.566
+cor.test(TemperMut$T_C.NormalOnlyByT,TemperMut$Temperature, method = 'spearman') # still positive!!!! rho = 0.285
+cor.test(TemperMut$T_A,TemperMut$Temperature, method = 'spearman')
+cor.test(TemperMut$T_G,TemperMut$Temperature, method = 'spearman') # negative
 
-cor.test(TemperMut$A_T,TemperMut$T..oC., method = 'spearman')
-cor.test(TemperMut$A_G,TemperMut$T..oC., method = 'spearman') # a bit negative
-cor.test(TemperMut$A_C,TemperMut$T..oC., method = 'spearman')
+cor.test(TemperMut$A_T,TemperMut$Temperature, method = 'spearman')
+cor.test(TemperMut$A_G,TemperMut$Temperature, method = 'spearman') # super negative
+cor.test(TemperMut$A_C,TemperMut$Temperature, method = 'spearman') # negative
 
-cor.test(TemperMut$G_A,TemperMut$T..oC., method = 'spearman')
-cor.test(TemperMut$G_T,TemperMut$T..oC., method = 'spearman')
-cor.test(TemperMut$G_C,TemperMut$T..oC., method = 'spearman')
+cor.test(TemperMut$G_A,TemperMut$Temperature, method = 'spearman')
+cor.test(TemperMut$G_T,TemperMut$Temperature, method = 'spearman') # negative
+cor.test(TemperMut$G_C,TemperMut$Temperature, method = 'spearman')
 
-cor.test(TemperMut$C_A,TemperMut$T..oC., method = 'spearman')
-cor.test(TemperMut$C_T,TemperMut$T..oC., method = 'spearman')
-cor.test(TemperMut$C_G,TemperMut$T..oC., method = 'spearman')
+cor.test(TemperMut$C_A,TemperMut$Temperature, method = 'spearman')
+cor.test(TemperMut$C_T,TemperMut$Temperature, method = 'spearman')
+cor.test(TemperMut$C_G,TemperMut$Temperature, method = 'spearman') # negative
 
-### temperature in fishes: from cold and tropical waters:
-ColdFish = read.table('../../Body/1Raw/cold_water_fishes.txt', sep = '\t', header = FALSE); names(ColdFish)=c('Species')
-ColdFish$Species = gsub(' ','_',ColdFish$Species)
+a <- lm(TemperMut$Temperature ~ TemperMut$T_C + TemperMut$T_G + TemperMut$A_G + TemperMut$A_C + TemperMut$G_T + TemperMut$C_G); summary(a)
+a <- lm(TemperMut$Temperature ~ TemperMut$T_C + TemperMut$A_G + TemperMut$A_C + TemperMut$G_T + TemperMut$C_G); summary(a)
+a <- lm(TemperMut$Temperature ~ TemperMut$T_C + TemperMut$A_G + TemperMut$G_T + TemperMut$C_G); summary(a)
+a <- lm(TemperMut$Temperature ~ TemperMut$T_C + TemperMut$G_T + TemperMut$C_G); summary(a)
+a <- lm(TemperMut$Temperature ~ scale(TemperMut$T_C) + scale(TemperMut$G_T) + scale(TemperMut$C_G)); summary(a)
+# (Intercept)           15.9097     0.2905  54.759  < 2e-16 ***
+# scale(TemperMut$T_C)   4.1528     0.3706  11.207  < 2e-16 ***
+# scale(TemperMut$G_T)   2.0217     0.4035   5.011 9.04e-07 ***
+# scale(TemperMut$C_G)  -1.3195     0.4166  -3.167  0.00169 ** 
+# r2 = 0.37
 
-WarmFish = read.table('../../Body/1Raw/tropical_water_fishes.txt', sep = '\t', header = FALSE); names(WarmFish)=c('Species')
-WarmFish$Species = gsub(' ','_',WarmFish$Species)
+###### Mass:
+cor.test(TemperMut$T_C,TemperMut$Mass, method = 'spearman')        # negative
+cor.test(TemperMut$T_A,TemperMut$Mass, method = 'spearman')        # negative
+cor.test(TemperMut$T_G,TemperMut$Mass, method = 'spearman')        # positive
 
-boxplot(MUT[MUT$Species %in% ColdFish$Species,]$G_A,MUT[MUT$Species %in% WarmFish$Species,]$G_A)
+cor.test(TemperMut$A_T,TemperMut$Mass, method = 'spearman')
+cor.test(TemperMut$A_G,TemperMut$Mass, method = 'spearman')
+cor.test(TemperMut$A_C,TemperMut$Mass, method = 'spearman')
 
+cor.test(TemperMut$G_A,TemperMut$Mass, method = 'spearman')
+cor.test(TemperMut$G_T,TemperMut$Mass, method = 'spearman')
+cor.test(TemperMut$G_C,TemperMut$Mass, method = 'spearman')
 
+cor.test(TemperMut$C_A,TemperMut$Mass, method = 'spearman')
+cor.test(TemperMut$C_T,TemperMut$Mass, method = 'spearman')
+cor.test(TemperMut$C_G,TemperMut$Mass, method = 'spearman')       # positive
 
+a <- lm(log2(TemperMut$Mass) ~ TemperMut$T_C + TemperMut$T_A + TemperMut$T_G + TemperMut$C_G); summary(a)
+a <- lm(log2(TemperMut$Mass) ~ TemperMut$T_C + TemperMut$T_A + TemperMut$T_G); summary(a)
+a <- lm(log2(TemperMut$Mass) ~ scale(TemperMut$T_C) + scale(TemperMut$T_A) + scale(TemperMut$T_G)); summary(a)
 
+# (Intercept)           7.80409    0.08928  87.414  < 2e-16 ***
+# scale(TemperMut$T_C) -0.35655    0.09264  -3.849 0.000144 ***
+# scale(TemperMut$T_A) -0.48299    0.09001  -5.366 1.56e-07 ***
+# scale(TemperMut$T_G)  0.39520    0.09274   4.261 2.68e-05 ***
+# r2 = 0.17
 
+###### MetabolRate:
+cor.test(TemperMut$T_C,TemperMut$MetabolRate, method = 'spearman')  # negative
+cor.test(TemperMut$T_A,TemperMut$MetabolRate, method = 'spearman')  # negative
+cor.test(TemperMut$T_G,TemperMut$MetabolRate, method = 'spearman')  # positive
 
+cor.test(TemperMut$A_T,TemperMut$MetabolRate, method = 'spearman')
+cor.test(TemperMut$A_G,TemperMut$MetabolRate, method = 'spearman')   # positive
+cor.test(TemperMut$A_C,TemperMut$MetabolRate, method = 'spearman')
 
+cor.test(TemperMut$G_A,TemperMut$MetabolRate, method = 'spearman')
+cor.test(TemperMut$G_T,TemperMut$MetabolRate, method = 'spearman')
+cor.test(TemperMut$G_C,TemperMut$MetabolRate, method = 'spearman')
 
+cor.test(TemperMut$C_A,TemperMut$MetabolRate, method = 'spearman')
+cor.test(TemperMut$C_T,TemperMut$MetabolRate, method = 'spearman')  # positive
+cor.test(TemperMut$C_G,TemperMut$MetabolRate, method = 'spearman')  # positive
+
+a <- lm(log2(TemperMut$MetabolRate) ~ TemperMut$T_C + TemperMut$T_A + TemperMut$T_G + TemperMut$A_G + TemperMut$C_T  + TemperMut$C_G); summary(a)
+a <- lm(log2(TemperMut$MetabolRate) ~ TemperMut$T_C + TemperMut$T_A + TemperMut$A_G + TemperMut$C_T  + TemperMut$C_G); summary(a)
+a <- lm(log2(TemperMut$MetabolRate) ~ TemperMut$T_A + TemperMut$A_G + TemperMut$C_T  + TemperMut$C_G); summary(a)
+a <- lm(log2(TemperMut$MetabolRate) ~ scale(TemperMut$T_A) + scale(TemperMut$A_G) + scale(TemperMut$C_T)  + scale(TemperMut$C_G)); summary(a)
+# (Intercept)           3.57036    0.07808  45.726  < 2e-16 ***
+# scale(TemperMut$T_A) -0.65710    0.11770  -5.583 5.11e-08 ***
+# scale(TemperMut$A_G) -0.33857    0.10539  -3.212  0.00145 ** 
+# scale(TemperMut$C_T)  0.27527    0.09305   2.958  0.00333 ** 
+# scale(TemperMut$C_G)  0.70015    0.11048   6.338 8.07e-10 ***
+# r2 = 0.14
--- Head/2Scripts/VertebratePolymorphisms.MutSpecComparisons.Analyses.Ecology.Mammals.R
+++ Head/2Scripts/VertebratePolymorphisms.MutSpecComparisons.Analyses.Ecology.Mammals.R
@@ -1,62 +1,96 @@
 rm(list=ls(all=TRUE))
 
-MUT = read.table('../../Body/3Results/VertebratePolymorphisms.MutSpecData.OnlyFourFoldDegAllGenes.txt', header = TRUE)
+pdf('../../Body/4Figures/VertebratePolymorphisms.MutSpecComparisons.Analyses.Ecology.Mammals.R.01.pdf')
+
+# MUT = read.table('../../Body/3Results/VertebratePolymorphisms.MutSpecData.OnlyFourFoldDegAllGenes.txt', header = TRUE)
+MUT = read.table('../../Body/3Results/VertebratePolymorphisms.MutSpecData.OnlyFourFoldDegCytb.txt', header = TRUE) # OnlyFourFoldDegCytb
+MUT$TsTv = (MUT$T_C + MUT$C_T + MUT$G_A + MUT$A_G) / (MUT$T_A + MUT$A_T + MUT$G_C + MUT$C_G + MUT$G_T + MUT$T_G + MUT$C_A + MUT$A_C)
+summary(MUT$TsTv)
+MUT = MUT[MUT$TsTv < Inf,]
 
 GT = read.table('../../Body/1Raw/GenerationLenghtforMammals.xlsx.txt', sep = '\t', header = TRUE)
 GT$Species = gsub(' ','_',GT$Scientific_name)
 
-#########
-#### MAMMALS: Generation length ~ T>C + A>G
-#########
+Temper = read.table('../../Body/1Raw/BodyParametersForAllClasses.txt', header = TRUE)
 
-MamGt = merge(GT,MUT, by = 'Species')
+MamGt = merge(GT,MUT, by = 'Species') # 426
+Qual = data.frame(table(MamGt$Species))
+UniqueSpecies = Qual[Qual$Freq == 1,]$Var1; length(UniqueSpecies);
+MamGt = MamGt[MamGt$Species %in% UniqueSpecies,]
 
-cor.test(MamGt$GenerationLength_d,MamGt$A_T, method = 'spearman') # negative
-cor.test(MamGt$GenerationLength_d,MamGt$A_G, method = 'spearman') # positive
-cor.test(MamGt$GenerationLength_d,MamGt$A_C, method = 'spearman') # negative
+######## TsTv and Generation length - significant
 
-cor.test(MamGt$GenerationLength_d,MamGt$T_C, method = 'spearman') # positive
+nrow(MamGt)  # 424
+cor.test(MamGt$TsTv,MamGt$GenerationLength_d, method = 'spearman') # rho = 0.228, p = 2.021e-06
+plot(log2(MamGt$GenerationLength_d),log2(MamGt$TsTv))
+
+#### Generation length ~ T>C + A>G
+
+cor.test(MamGt$GenerationLength_d,MamGt$A_T, method = 'spearman') # negative, - 0.22
+cor.test(MamGt$GenerationLength_d,MamGt$A_G, method = 'spearman') 
+cor.test(MamGt$GenerationLength_d,MamGt$A_C, method = 'spearman') # negative, - 0.17
+
+cor.test(MamGt$GenerationLength_d,MamGt$T_C, method = 'spearman') # positive, rhp = 0.25
 cor.test(MamGt$GenerationLength_d,MamGt$T_A, method = 'spearman')
-cor.test(MamGt$GenerationLength_d,MamGt$T_G, method = 'spearman')
+cor.test(MamGt$GenerationLength_d,MamGt$T_G, method = 'spearman') 
 
 cor.test(MamGt$GenerationLength_d,MamGt$G_A, method = 'spearman')
-cor.test(MamGt$GenerationLength_d,MamGt$G_T, method = 'spearman') # negative
-cor.test(MamGt$GenerationLength_d,MamGt$G_C, method = 'spearman')
+cor.test(MamGt$GenerationLength_d,MamGt$G_T, method = 'spearman') # negative, -0.27
+cor.test(MamGt$GenerationLength_d,MamGt$G_C, method = 'spearman') 
 
-cor.test(MamGt$GenerationLength_d,MamGt$C_A, method = 'spearman') # negative
+cor.test(MamGt$GenerationLength_d,MamGt$C_A, method = 'spearman') # negative, - 0.23
 cor.test(MamGt$GenerationLength_d,MamGt$C_T, method = 'spearman')
 cor.test(MamGt$GenerationLength_d,MamGt$C_G, method = 'spearman')
 
-a<-lm(MamGt$GenerationLength_d ~ MamGt$A_T +  MamGt$A_G + MamGt$A_C + MamGt$T_C + MamGt$G_T + MamGt$C_A); summary(a)
-a<-lm(MamGt$GenerationLength_d ~ MamGt$A_T +  MamGt$A_G + MamGt$A_C + MamGt$T_C + MamGt$G_T); summary(a)
-a<-lm(scale(MamGt$GenerationLength_d) ~ scale(MamGt$A_T) +  scale(MamGt$A_G) + scale(MamGt$A_C) + scale(MamGt$T_C) + scale(MamGt$G_T)); summary(a)
-a<-lm(MamGt$GenerationLength_d ~ scale(MamGt$A_T) +  scale(MamGt$A_G) + scale(MamGt$A_C) + scale(MamGt$T_C) + scale(MamGt$G_T)); summary(a)
-#  (Intercept)       2002.92      75.94  26.376  < 2e-16 ***
-#  scale(MamGt$A_T)  -254.80      78.19  -3.259  0.00120 ** 
-#  scale(MamGt$A_G)   345.73      78.19   4.422 1.22e-05 *** !!!
-#  scale(MamGt$A_C)  -239.33      79.14  -3.024  0.00263 ** 
-#  scale(MamGt$T_C)   326.73      80.25   4.071 5.48e-05 *** !!!
-#  scale(MamGt$G_T)   256.02      78.44   3.264  0.00118 ** 
+a<-lm(MamGt$GenerationLength_d ~ MamGt$A_T +  MamGt$A_G + MamGt$A_C + MamGt$T_C + MamGt$T_G + MamGt$G_T + MamGt$C_A); summary(a) # T>C is the most significant
+a<-lm(MamGt$GenerationLength_d ~ MamGt$A_T +  MamGt$A_G + MamGt$A_C + MamGt$T_C + MamGt$G_T + MamGt$C_A); summary(a) # T>C is the most significant
+a<-lm(MamGt$GenerationLength_d ~ MamGt$A_T +  MamGt$A_G + MamGt$T_C + MamGt$G_T + MamGt$C_A); summary(a) # T>C is the most significant
+a<-lm(MamGt$GenerationLength_d ~ MamGt$A_T +  MamGt$T_C + MamGt$G_T + MamGt$C_A); summary(a) # T>C is the most significant
+a<-lm(MamGt$GenerationLength_d ~ MamGt$A_T +  MamGt$T_C + MamGt$G_T); summary(a) # T>C is the most significant
+a<-lm(log2(MamGt$GenerationLength_d) ~ scale(MamGt$A_T) +  scale(MamGt$T_C) + scale(MamGt$G_T)); summary(a) # T>C is the most significant#
+# (Intercept)      10.39025    0.05584 186.064  < 2e-16 ***
+#  scale(MamGt$A_T) -0.18037    0.05603  -3.219  0.00138 ** 
+#  scale(MamGt$T_C)  0.26205    0.05734   4.570 6.42e-06 ***
+#  scale(MamGt$G_T) -0.18386    0.05723  -3.213  0.00142 ** 
+  ---
 
 ### remove effect of ancestral nucleotide frequency (WORKS!!!!!)
 
 MamGt$T_C.NoEffectOfTFreq = MamGt$T_C / (MamGt$T_C + MamGt$T_A + MamGt$T_G); summary(MamGt$T_C.NoEffectOfTFreq)
-cor.test(MamGt$GenerationLength_d,MamGt$T_C.NoEffectOfTFreq, method = 'spearman') # positive and significant!!!!
+cor.test(MamGt$GenerationLength_d,MamGt$T_C.NoEffectOfTFreq, method = 'spearman') # positive and significant!!!! rho = 0.164, p = 0.0007114
 
-MamGt$A_G.NoEffectOfAFreq = MamGt$A_G / (MamGt$A_G + MamGt$A_T + MamGt$A_C); summary(MamGt$A_G.NoEffectOfAFreq)
-cor.test(MamGt$GenerationLength_d,MamGt$A_G.NoEffectOfAFreq, method = 'spearman') # super positive and super significant!!!!
-
-par(mfrow=c(2,2))
 plot(log2(MamGt$GenerationLength_d),MamGt$T_C)
-plot(log2(MamGt$GenerationLength_d),MamGt$A_G)
+
+##### Body Mass (N = 426)
+
+cor.test(MamGt$AdultBodyMass_g,MamGt$A_T, method = 'spearman')
+cor.test(MamGt$AdultBodyMass_g,MamGt$A_C, method = 'spearman') # negative
+cor.test(MamGt$AdultBodyMass_g,MamGt$A_G, method = 'spearman')
+
+cor.test(MamGt$AdultBodyMass_g,MamGt$T_A, method = 'spearman')
+cor.test(MamGt$AdultBodyMass_g,MamGt$T_G, method = 'spearman')
+cor.test(MamGt$AdultBodyMass_g,MamGt$T_C, method = 'spearman') # positive
+
+cor.test(MamGt$AdultBodyMass_g,MamGt$G_A, method = 'spearman')
+cor.test(MamGt$AdultBodyMass_g,MamGt$G_T, method = 'spearman') # negative
+cor.test(MamGt$AdultBodyMass_g,MamGt$G_C, method = 'spearman')
+
+cor.test(MamGt$AdultBodyMass_g,MamGt$C_A, method = 'spearman') # negative
+cor.test(MamGt$AdultBodyMass_g,MamGt$C_T, method = 'spearman')
+cor.test(MamGt$AdultBodyMass_g,MamGt$C_G, method = 'spearman')
+
+a<-lm(log2(MamGt$AdultBodyMass_g) ~ MamGt$A_C + MamGt$T_C + MamGt$G_T + MamGt$C_A); summary(a)   # T>C is the most significant
+a<-lm(log2(MamGt$AdultBodyMass_g) ~ MamGt$T_C + MamGt$G_T + MamGt$C_A); summary(a)   # T>C is the most significant
+a<-lm(log2(MamGt$AdultBodyMass_g) ~ MamGt$T_C + MamGt$C_A); summary(a)   # T>C is the most significant
+a<-lm(log2(MamGt$AdultBodyMass_g) ~ scale(MamGt$T_C) + scale(MamGt$C_A)); summary(a)   # T>C is the most significant
+
 
 #########
-#### MAMMALS: Body Temperature  ~ NOTHING (N = 84)
+#### MAMMALS: Body Temperature  (N = 84) - nothing
 #########
 
-Temper = read.table('../../Body/1Raw/MammalianTemperature.ods.txt', sep = '\t', header = TRUE)
-Temper$Species = paste(Temper$FirstName,Temper$SecondName,sep = '_')
-
+table(Temper$Class)
+Temper = Temper[Temper$Class == 'Mammalia',]
 MamTemper = merge(Temper,MUT, by = 'Species') # 84 just
 
 cor.test(MamTemper$Temp,MamTemper$A_T, method = 'spearman')
@@ -65,40 +99,12 @@ cor.test(MamTemper$Temp,MamTemper$A_C, method = 'spearman') #  a bit negative
 cor.test(MamTemper$Temp,MamTemper$T_A, method = 'spearman')
 cor.test(MamTemper$Temp,MamTemper$T_G, method = 'spearman')
 cor.test(MamTemper$Temp,MamTemper$T_C, method = 'spearman')
-cor.test(MamTemper$Temp,MamTemper$G_A, method = 'spearman'); plot(MamTemper$Temp,MamTemper$G_A)
+cor.test(MamTemper$Temp,MamTemper$G_A, method = 'spearman')
 cor.test(MamTemper$Temp,MamTemper$G_T, method = 'spearman')
 cor.test(MamTemper$Temp,MamTemper$G_C, method = 'spearman')
 cor.test(MamTemper$Temp,MamTemper$C_A, method = 'spearman')
 cor.test(MamTemper$Temp,MamTemper$C_T, method = 'spearman')
 cor.test(MamTemper$Temp,MamTemper$C_G, method = 'spearman')
-
-#########
-#### MAMMALS: MR  ~ 
-#########
-
-Temper = read.table('../../Body/1Raw/MammalianTemperature.ods.txt', sep = '\t', header = TRUE)
-Temper$Species = paste(Temper$FirstName,Temper$SecondName,sep = '_')
-
-MamTemper = merge(Temper,MUT, by = 'Species') # 84 just
-
-cor.test(MamTemper$G_A,MamTemper$MR, method = 'spearman'); # a bit negative (non significant)
-cor.test(MamTemper$G_T,MamTemper$MR, method = 'spearman');
-cor.test(MamTemper$G_C,MamTemper$MR, method = 'spearman');
-
-cor.test(MamTemper$A_T,MamTemper$MR, method = 'spearman');
-cor.test(MamTemper$A_G,MamTemper$MR, method = 'spearman');
-cor.test(MamTemper$A_C,MamTemper$MR, method = 'spearman');
-
-cor.test(MamTemper$T_A,MamTemper$MR, method = 'spearman');
-cor.test(MamTemper$T_G,MamTemper$MR, method = 'spearman');
-cor.test(MamTemper$T_C,MamTemper$MR, method = 'spearman');
-
-cor.test(MamTemper$C_A,MamTemper$MR, method = 'spearman'); # a bit negative
-cor.test(MamTemper$C_T,MamTemper$MR, method = 'spearman'); # a bit positive
-cor.test(MamTemper$C_G,MamTemper$MR, method = 'spearman');
-
-plot(MamTemper$Temp,MamTemper$G_A)
-
 
 
 #########
@@ -110,15 +116,9 @@ names(Hib) = c('Species')
 Hib$Species = gsub(' ','_',Hib$Species)
 
 MamGt$G_A.NoEffectOfGFreq = MamGt$G_A / (MamGt$G_A + MamGt$G_T + MamGt$G_C); summary(MamGt$G_A.NoEffectOfGFreq)
-
 nrow(MamGt[MamGt$Species %in% Hib$Species,]) # N = 35
-
-boxplot(MamGt[MamGt$Species %in% Hib$Species,]$G_A,MamGt[!MamGt$Species %in% Hib$Species,]$G_A, outline = FALSE, notch = TRUE)
-boxplot(MamGt[MamGt$Species %in% Hib$Species,]$G_A.NoEffectOfGFreq,MamGt[!MamGt$Species %in% Hib$Species,]$G_A.NoEffectOfGFreq, outline = FALSE, notch = TRUE)
 MamGt = MamGt[MamGt$G_A.NoEffectOfGFreq < 1,]
-boxplot(MamGt[MamGt$Species %in% Hib$Species,]$G_A.NoEffectOfGFreq,MamGt[!MamGt$Species %in% Hib$Species,]$G_A.NoEffectOfGFreq, outline = FALSE, notch = TRUE)
 wilcox.test(MamGt[MamGt$Species %in% Hib$Species,]$G_A.NoEffectOfGFreq,MamGt[!MamGt$Species %in% Hib$Species,]$G_A.NoEffectOfGFreq, alternative = 'less')
-
 MamGtTemper = merge(MamGt,Temper) # 78
 a<-lm(MamGtTemper$G_A ~ MamGtTemper$GenerationLength_d*MamGtTemper$Temp); summary(a)
 
@@ -135,17 +135,70 @@ cor.test(AnAgeMammals$G_A,AnAgeMammals$Temperature..K., method = 'spearman')
 cor.test(AnAgeMammals$G_A,AnAgeMammals$Metabolic.rate..W., method = 'spearman') # a bit negative
 cor.test(AnAgeMammals$A_G,AnAgeMammals$Metabolic.rate..W., method = 'spearman') #
 cor.test(AnAgeMammals$T_C,AnAgeMammals$Metabolic.rate..W., method = 'spearman') # a bit positive
-
 cor.test(AnAgeMammals$G_A,AnAgeMammals$Body.mass..g., method = 'spearman') # a bit negative!
 
+
 #########
-#### MAMMALS: Body Mass 
+#### MAMMALS: PRCOMP
 #########
 
+MUT = read.table('../../Body/3Results/VertebratePolymorphisms.MutSpecData.OnlyFourFoldDegCytb.txt', header = TRUE) # OnlyFourFoldDegCytb
+MUT$TsTv = (MUT$T_C + MUT$C_T + MUT$G_A + MUT$A_G) / (MUT$T_A + MUT$A_T + MUT$G_C + MUT$C_G + MUT$G_T + MUT$T_G + MUT$C_A + MUT$A_C)
+MUT = MUT[MUT$TsTv < Inf,]
 GT = read.table('../../Body/1Raw/GenerationLenghtforMammals.xlsx.txt', sep = '\t', header = TRUE)
 GT$Species = gsub(' ','_',GT$Scientific_name)
+MamGt = merge(GT,MUT, by = 'Species') # 426
 
-MamGt = merge(GT,MUT, by = 'Species')
+Qual = data.frame(table(MamGt$Species))
+UniqueSpecies = Qual[Qual$Freq == 1,]$Var1; length(UniqueSpecies);
+MamGt = MamGt[MamGt$Species %in% UniqueSpecies,]
 
-cor.test(MamGt$G_A,MamGt$AdultBodyMass_g, method = 'spearman') # nothing (negative sign)
-cor.test(MamGt$T_C,MamGt$AdultBodyMass_g, method = 'spearman') # a bit positive
+###### PCA 
+
+names(MamGt)
+MATRIX = MamGt[,c(14:25)]
+row.names(MATRIX)=MamGt$Species
+matrix = MATRIX
+
+PCA = prcomp(matrix, center = TRUE, scale = TRUE) #FALSE) # I don't scale because we analyze the same units (fraction from MutSpec) 
+print(PCA)  
+summary(PCA)
+MATRIX$Pca1 = PCA$x[,1]
+MATRIX$Pca2 = PCA$x[,2]
+MATRIX$Pca3 = PCA$x[,3]
+MATRIX$Pca4 = PCA$x[,4]
+
+MATRIX = cbind(MATRIX,MamGt[,c(1:14,26)])
+
+###### ANALYSIS OF COMPONENTS
+plot(log2(MATRIX$GenerationLength_d),MATRIX$Pca2)
+cor.test(MATRIX$Pca1,MATRIX$GenerationLength_d, method = 'spearman') # rho = 0.004288973, p = 0.9298
+cor.test(MATRIX$Pca2,MATRIX$GenerationLength_d, method = 'spearman') # rho = -0.3897472, p =  < 2.2e-16
+cor.test(MATRIX$Pca3,MATRIX$GenerationLength_d, method = 'spearman') # rho = -0.3897472, p =  < 2.2e-16
+
+biplot(PCA, choices=c(1,2), col = c('white','black'), cex = 0.8) #  biplot(princomp(USArrests),choices=c(1,3))
+
+MATRIX$SIZE = (log10(MATRIX$GenerationLength_d) - min(log10(MATRIX$GenerationLength_d))) / (max(log10(MATRIX$GenerationLength_d))   - min(log10(MATRIX$GenerationLength_d)))  #  normilaze data to 0-1 range
+summary(MATRIX$SIZE)
+
+plot(MATRIX$Pca1,MATRIX$Pca2, pch = 16, cex = 1.5, col = rgb(MATRIX$SIZE,0,0,1))
+plot(MATRIX$Pca1,MATRIX$Pca2, pch = 16, cex = 1.5, col = rgb(0,MATRIX$SIZE,0,1), xlim = c(-11,3), ylim = c(-3,4.5)); 
+biplot(PCA, choices=c(1,2), scale = 0.5, col = c('grey','black'), cex = 0.5) # , xlim = c(-11,3), ylim = c(-3,4.5))
+
+summary(MATRIX$Pca1)
+summary(PCA$x[,1])
+
+# arrows(0,0,0.50722224,0.078411901, col = 'red', lwd = 2)
+
+biplot(PCA, choices=c(1,2), scale = 1, col = c('grey','black'), cex = 0.5)
+plot(MATRIX$Pca1,MATRIX$Pca2, pch = 16, cex = 1.5, col = rgb(0,0,MATRIX$SIZE,1))
+
+plot(PCA$x[,1],PCA$x[,2])
+
+PCA$x # PC's
+PCA$sdev # the eigenvalues (res$sdev) giving information on the magnitude of each PC, 
+PCA$rotation # and the loadings (res$rotation).
+
+dev.off()
+
+
