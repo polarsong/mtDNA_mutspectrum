5991636fe7b375fda38d3353e4aa9c7f67e3abb3
Alya <polarsong4@gmail.com>
2020-01-19 19:36:56 +0200

    added summary

--- Head/2Scripts/VertebratePolymorphisms.MutSpecComparisons.Analyses.Ecology.Actinopterygii.FishBaseData.Maturity~Temp.R
+++ Head/2Scripts/VertebratePolymorphisms.MutSpecComparisons.Analyses.Ecology.Actinopterygii.FishBaseData.Maturity~Temp.R
@@ -33,4 +33,23 @@ allparameters=merge(averageTemperMut, MATU)
 
 test= lm(T_C ~ Temperature + Tm, data=allparameters)
 
-plot(test)
+summary(test)
+
+#####Call:
+#lm(formula = T_C ~ Temperature + Tm, data = allparameters)
+
+#Residuals:
+#  Min       1Q   Median       3Q      Max 
+#-0.11116 -0.03176 -0.01421  0.02424  0.16122 
+
+#Coefficients:
+#  Estimate Std. Error t value Pr(>|t|)    
+#(Intercept)  0.0150943  0.0145573   1.037   0.3014    
+#Temperature  0.0064884  0.0007277   8.916 1.04e-15 ***
+#  Tm          -0.0036721  0.0018461  -1.989   0.0484 *  
+#  ---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+#Residual standard error: 0.04324 on 160 degrees of freedom
+#Multiple R-squared:  0.3982,	Adjusted R-squared:  0.3907 
+#F-statistic: 52.94 on 2 and 160 DF,  p-value: < 2.2e-16
