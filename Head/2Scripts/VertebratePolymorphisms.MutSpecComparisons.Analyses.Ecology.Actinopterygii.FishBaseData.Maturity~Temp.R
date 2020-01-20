3ba07a045921f1a47a3d489baf6bc9167844d8f8
Alya <polarsong4@gmail.com>
2020-01-20 18:45:11 +0200

    Update VertebratePolymorphisms.MutSpecComparisons.Analyses.Ecology.Actinopterygii.FishBaseData.Maturity~Temp.R

--- Head/2Scripts/VertebratePolymorphisms.MutSpecComparisons.Analyses.Ecology.Actinopterygii.FishBaseData.Maturity~Temp.R
+++ Head/2Scripts/VertebratePolymorphisms.MutSpecComparisons.Analyses.Ecology.Actinopterygii.FishBaseData.Maturity~Temp.R
@@ -1,6 +1,8 @@
 rm(list=ls(all=TRUE))
 
 
+
+
 ###TEMPERATURE another table
 MUT = read.table('../../Body/3Results/VertebratePolymorphisms.MutSpecData.OnlyFourFoldDegAllGenes.txt', header = TRUE)
 class(MUT$A_T)
@@ -22,34 +24,35 @@ averageTemperMut = merge(MUT,averageTEMPE)
 MATU = read.table('../../Body/1Raw/FishBaseLmTm.txt')
 colnames(MATU)=c("Species", "Lm", "Tm")
 MATU$Species=gsub(" ", "_", MATU$Species)
-MATU=MATU[!is.na(MATU$Tm),]
-MATU=MATU[MATU$Lm != "",]                                                            #maturity table
-
+MATU=MATU[!is.na(MATU$Tm),]                           #maturity table
+MATU$Lm = NULL
+MATU = aggregate(Tm ~ ., mean, data = MATU) 
 
 ##multiple reg 
 
 allparameters=merge(averageTemperMut, MATU)
 
 
-test= lm(T_C ~ Temperature + Tm, data=allparameters)
-
-summary(test)
-
-#####Call:
-#lm(formula = T_C ~ Temperature + Tm, data = allparameters)
+ltest = lm(formula = T_C ~ scale(Temperature)*scale(Tm), data = allparameters)
+summary(ltest)
+#Call:
+#lm(formula = T_C ~ scale(Temperature) * scale(Tm), data = allparameters)
 
 #Residuals:
-#  Min       1Q   Median       3Q      Max 
-#-0.11116 -0.03176 -0.01421  0.02424  0.16122 
+#  Min        1Q    Median        3Q       Max 
+#-0.126180 -0.039390 -0.002721  0.029179  0.162737 
 
 #Coefficients:
 #  Estimate Std. Error t value Pr(>|t|)    
-#(Intercept)  0.0150943  0.0145573   1.037   0.3014    
-#Temperature  0.0064884  0.0007277   8.916 1.04e-15 ***
-#  Tm          -0.0036721  0.0018461  -1.989   0.0484 *  
-#  ---
+#(Intercept)                   0.121096   0.010621  11.401 3.11e-12 ***
+#  scale(Temperature)            0.025915   0.010736   2.414   0.0223 *  
+#  scale(Tm)                    -0.008237   0.010698  -0.770   0.4476    
+#scale(Temperature):scale(Tm)  0.011877   0.014888   0.798   0.4315    
+#---
 #  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
-#Residual standard error: 0.04324 on 160 degrees of freedom
-#Multiple R-squared:  0.3982,	Adjusted R-squared:  0.3907 
-#F-statistic: 52.94 on 2 and 160 DF,  p-value: < 2.2e-16
+#Residual standard error: 0.05871 on 29 degrees of freedom
+#Multiple R-squared:  0.2016,	Adjusted R-squared:  0.119 
+#F-statistic: 2.441 on 3 and 29 DF,  p-value: 0.08438
+
+
