aece1ab2cc584ef4cfe0af565530bb0276124e47
Alina Mikhailova <mihailovaalina777@yandex.ru>
2021-01-15 13:32:32 +0100

    Counted amino acid frequencies for all chordates in 12 genes

---
+++ Head/2Scripts/WholeGenomeAnalyses.AminoAcidFreqsChordata.R
@@ -0,0 +1,34 @@
+library(dplyr)
+library(Biostrings)
+
+codons = read.table('~/Desktop/AllGenesCodonUsageNoOverlap.txt', header = TRUE, sep='\t')
+
+codons$AminoNoOverlap = as.character(codons$AminoNoOverlap)
+codons$Species = as.character(codons$Species)
+codons$Gene = as.character(codons$Gene)
+
+aa = codons %>%
+  select(Species, AminoNoOverlap, Taxonomy, Class, Gene) %>%
+  filter(Gene != 'ND6')
+
+
+# as.data.frame(t(alphabetFrequency(AAString(aa$AminoNoOverlap[1000]))))
+# as.data.frame(t(alphabetFrequency(AAString(aa$AminoNoOverlap[100]))))
+
+df <- data.frame(matrix(ncol = 33, nrow = 0))
+
+for(i in 1:nrow(aa)){
+  # i = 1
+  seq = AAString(aa$AminoNoOverlap[i])
+  temp_df = as.data.frame(t(c(aa$Species[i], aa$Gene[i], t(alphabetFrequency(seq)))))
+  df = rbind(df, temp_df)
+}
+
+names(df) = c('Species', 'Gene', names(as.data.frame(t(alphabetFrequency(seq)))))
+three_letter = c('Ala', 'Arg', 'Asn', 'Asp', 'Cys', 'Gln', 'Glu', 'Gly', 'His', 'Ile', 'Leu', 'Lys', 'Met', 'Phe', 'Pro', 'Ser', 'Thr', 'Trp', 'Tyr', 'Val')
+
+names(df)[3:22] = three_letter
+
+final = distinct(inner_join(df, codons[, c('Species', 'Class')]))
+
+write.csv(final, '~/Alina/Kostya/AminoAcidFreqsChordata.csv', quote = FALSE, row.names = FALSE)
