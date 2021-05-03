ae145eddbd2296cda21d30c8ab790f9f8cf1508a
YuryGusarov <77166688+YuryGusarov@users.noreply.github.com>
2021-05-03 16:55:20 +0200

    Cool fliers analyses

--- Head/2Scripts/Tropical and high altitude birds.R
+++ Head/2Scripts/Tropical and high altitude birds.R
@@ -263,5 +263,10 @@ boxplot(mutspec_and_pheno[mutspec_and_pheno$Phenotype == ",\"TROPIC\"",]$transit
         mutspec_and_pheno[mutspec_and_pheno$Phenotype == ",\"HI\"",]$transitions_on_transverions, mutspec_and_pheno[mutspec_and_pheno$Phenotype == ",\"FM\"",]$transitions_on_transverions, 
         mutspec_and_pheno[mutspec_and_pheno$Phenotype == ",\"NF\"",]$transitions_on_transverions, mutspec_and_pheno[mutspec_and_pheno$Phenotype == ",\"DI\"",]$transitions_on_transverions,
         mutspec_and_pheno[mutspec_and_pheno$Phenotype == ",\"AI\"",]$transitions_on_transverions, names=c('Trop14','Wint7','HiAlt3', 'LD10', 'NF1', 'DI1', 'AI5'), ylab = 'transitions_on_transverions', notch = TRUE, outline = FALSE)
+dev.off()
 
+#AI against ALL
+pdf("../../Body/3Results/birds boxplots mutspec transitions on transvertions all against cool fliers.pdf")
+boxplot(mutspec_and_pheno[mutspec_and_pheno$Phenotype == ",\"AI\"",]$transitions_on_transverions, mutspec_and_pheno[mutspec_and_pheno$Phenotype != ",\"AI\"",]$transitions_on_transverions, names = c('Outstanding flight abilities', 'All others'), ylab = 'transitions_on_transvertions')
+boxplot(mutspec_and_pheno[mutspec_and_pheno$Phenotype == ",\"AI\"",]$transitions_on_transverions, mutspec_and_pheno[mutspec_and_pheno$Phenotype != ",\"AI\"",]$transitions_on_transverions, names = c('Outstanding flight abilities', 'All others'), ylab = 'transitions_on_transvertions', notch = TRUE, outline = FALSE)
 dev.off()
