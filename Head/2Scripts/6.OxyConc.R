d85f5158275d7d02b24269edefe1005a6f231032
diliushchenko <serioussammdk@gmail.com>
2021-10-11 13:54:51 +0200

    add numbers on species

--- Head/2Scripts/6.OxyConc.R
+++ Head/2Scripts/6.OxyConc.R
@@ -24,9 +24,9 @@ MD_standard = MD_standard %>%
 
 nrow(MD_standard) ### 69 Species for standard
 
-muttemp = read.table('../../Body/2Derived/Supplementary_table_2.txt', header =TRUE)
+muttemp = read.table('../../Body/2Derived/Supplementary_file_2.txt', header =TRUE)
 
-MD_mut_standard = merge(muttemp, MD_standard, by.y ='Spece', by.x ='Species') # only 20 sps left ;(
+MD_mut_standard = merge(muttemp, MD_standard, by.y ='Spece', by.x ='Species') # only 10 sps left ;(
 
 MD_mut_standard = MD_mut_standard %>%  select(-c(14,15,16))
 
@@ -35,34 +35,31 @@ MD_mut_standard$AGTC = MD_mut_standard$T_C / MD_mut_standard$A_G
 
 cor.test(MD_mut_standard$median.oxyconc , MD_mut_standard$A_G, method = 'spearman') ### p = 0.49
 
-pdf('../../Body/4Figures/Supplementary_figure_5.pdf')
-ggplot(MD_mut_standard, aes(x = median.oxyconc, y = A_G))+
+pdf('../../Body/4Figures/Supplementary_figure_7.pdf')
+ggplot(MD_mut_standard, aes(x = median.oxyconc, y = A_G, label = 1:10))+
   geom_point()+
   theme_bw()+
+  geom_text(hjust=-0.40, vjust=-0.40)+
   labs(x = 'Median Oxygen Consumption', y = 'Th to Ch')+
-  geom_smooth(method ="lm", color = 'red',size = 0.6, se =F)+
-  geom_text(x = 325 , y = 0.115 , label = 'p-value = 0.49, rho = -0.25', size = 5)+
-  geom_text(x = 325 , y = 0.11 , label = 'standard', size = 5)
+  geom_smooth(method ="lm", color = 'red',size = 0.6, se =F)
 
 cor.test(MD_mut_standard$median.oxyconc , MD_mut_standard$T_C, method = 'spearman') ### p  = 0.068 still bad
 
-ggplot(MD_mut_standard, aes(x = median.oxyconc, y = T_C))+
+ggplot(MD_mut_standard, aes(x = median.oxyconc, y = T_C, label = 1:10))+
   geom_point()+
   theme_bw()+
   labs(x = 'Median Oxygen Consumption', y = 'Ah to Gh')+
   geom_smooth(method ="lm", color = 'red',size = 0.6, se =F)+
-  geom_text(x = 325 , y = 0.115 , label = 'p-value = 0.068, rho = 0.82', size = 5)+
-  geom_text(x = 325 , y = 0.11 , label = 'standard', size = 5)
-
+  geom_text(hjust=1.3, vjust=-0.20)
+  
 cor.test(MD_mut_standard$median.oxyconc , MD_mut_standard$AGTC, method = 'spearman') ### p  = 0.14 still bad
 
-ggplot(MD_mut_standard, aes(x = median.oxyconc, y = AGTC))+
+ggplot(MD_mut_standard, aes(x = median.oxyconc, y = AGTC, label= 1:10))+
   geom_point()+
   theme_bw()+
   labs(x = 'Median Oxygen Consumption', y = 'A G / T C')+
   geom_smooth(method ="lm", color = 'red',size = 0.6, se =F)+
-  geom_text(x = 325 , y = 4 , label = 'p-value = 0.068, rho = 0.82', size = 5)+
-  geom_text(x = 325 , y = 3.2 , label = 'standard', size = 5)
+  geom_text(hjust=-0.4, vjust=0.50)
 
 dev.off()
 
