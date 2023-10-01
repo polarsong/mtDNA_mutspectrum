92394e3674f3649576a6b60d89d809ea1512002e
YuryGusarov <yurgusguss@mail.ru>
2023-10-01 18:44:47 +0200

    To think

--- Head/2Scripts/PCM_in_R_implementations/Birds_phylo_PCA_new.R
+++ Head/2Scripts/PCM_in_R_implementations/Birds_phylo_PCA_new.R
@@ -72,8 +72,7 @@ df_temp_fly$AnnualPrecip = log10(df_temp_fly$AnnualPrecip)
 df_temp_fly$PrecipRange = log10(df_temp_fly$PrecipRange)
 
 #SET ALMOST EVERYTHING TO LOG10
-temp_birds_pca<-phyl.pca(temp_tree,df_temp_fly[,c(13:14)])
-temp_birds_pca
+temp_birds_pca<-phyl.pca(temp_tree,df_temp_fly[,c(2:16, 18)])
 par(mar=c(4.1,4.1,2.1,1.1),las=1) ## set margins
 plot(temp_birds_pca,main="")
 #flying_birds_pca$Evec[,1]<--flying_birds_pca$Evec[,1]
@@ -95,7 +94,7 @@ eco<-setNames(df_temp_fly[,17],rownames(df_temp_fly))
 
 ECO<-to.matrix(eco,levels(eco))
 tiplabels(pie=ECO[temp_tree$tip.label,],cex=0.3)
-legend(x="topright",legend=levels(eco),cex=0.6,pch=21,
+legend(x="bottomleft",legend=levels(eco),cex=0.6,pch=21,
        pt.bg=rainbow(n=length(levels(eco))),pt.cex=1.5)
 
 a = as.data.frame(temp_birds_pca$S)
