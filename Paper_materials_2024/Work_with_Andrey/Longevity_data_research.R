8e4c15526330936af54262d0f9bc50435ee11454
YuryGusarov <yurgusguss@mail.ru>
2024-02-01 18:45:26 +0200

    Models run

--- Paper_materials_2024/Work_with_Andrey/Longevity_data_research.R
+++ Paper_materials_2024/Work_with_Andrey/Longevity_data_research.R
@@ -10,12 +10,4 @@ library(ggtree)
 library(stringr)
 library(dplyr)
 library(RSQLite)
-rangeMapper <- "rangeMapper_ecog.00929.sqlite"
-sqlite.driver <- dbDriver("SQLite")
-db <- dbConnect(sqlite.driver,
-                dbname = rangeMapper)
-
-dbListTables(db) #Выводит список таблиц в SQL базе
-BIO_Life_History <- dbReadTable(db, "BIO_Life_History")
-write.csv2(BIO_Life_History, file="BIO_Life_History.csv")
-if (file.exists("rangeMapper_ecog.00929.sqlite")) file.remove("rangeMapper_ecog.00929.sqlite")
+df_long = read.csv('BIO_Life_History.csv', header = TRUE, sep = ';')
--- Paper_materials_2024/Work_with_Andrey/Looking_at_datasets.R
+++ Paper_materials_2024/Work_with_Andrey/Looking_at_datasets.R
@@ -18,16 +18,18 @@ df_mtdna_orders$taxonomy = sub('.*\\ ', '', df_mtdna_orders$taxonomy)
 df_mtdna_orders$taxonomy = paste(df_mtdna_orders$taxonomy, 'formes', sep = '')
 names(df_mtdna_orders) = c('Species', 'Orders')
 names_v = unique(df_mtdna$species_name)
+df_mtdna$Mass = as.numeric(as.character(df_mtdna$Mass))
 df_short = data.frame()
 for (i in names_v)
 {
   df1 = df_mtdna[df_mtdna$species_name == i,]
   a = sum(df1$ghahSkew)/12
   b = sum(df1$chthSkew)/12
-  ab = c(i, a, b)
+  v = sum(df1$Mass)/12
+  ab = c(i, a, b, v)
   df_short = rbind(df_short, ab)
 }
-names(df_short) = c('Species', 'GhAhSkew', 'ThChSkew')
+names(df_short) = c('Species', 'GhAhSkew', 'ThChSkew', 'Mass')
 df_short$GhAhSkew = as.numeric(df_short$GhAhSkew)
 
 df12_bmr = df12[df12$Trait == 'BMR',]
@@ -57,7 +59,8 @@ ggplot(df_short2, aes(x = log10(BMR_value), y = GhAhSkew))+
   geom_smooth(method = 'lm', formula = y~x)+                            
   annotate('text', x = (min(log10(df_short2$BMR_value))+1), y = min(df_short2$GhAhSkew), label = round(abc1, digits = 4))
 df_short2$log10BMR = log10(df_short2$BMR_value)
-
+df_short2$Mass = as.numeric(df_short2$Mass)
+df_short2$log10Mass = log10(df_short2$Mass)
 counter = 0
 for (i in unique(df_short2$Orders))
 {
@@ -78,6 +81,47 @@ pdf('Birds_BMR.pdf',
 ggarrange(Order_Accipitriformes, Order_Anseriformes, Order_Apodiformes, Order_Apterygiformes, Order_Casuariiformes, Order_Charadriiformes, Order_Coliiformes, Order_Columbiformes, Order_Coraciiformes, Order_Cuculiformes, Order_Falconiformes, Order_Galliformes, Order_Gruiformes, Order_Passeriformes, Order_Pelecaniformes, Order_Piciformes, Order_Procellariiformes, Order_Psittaciformes, Order_Sphenisciformes, Order_Strigiformes, Order_Struthioniformes, Order_Tinamiformes, Order_Upupiformes, 
           ncol = 3, nrow = 11)
 dev.off()
+
+lmMassBMR = lm(GhAhSkew~log10Mass+log10BMR , data=df_short2)
+summary(lmMassBMR)
+res = resid(lmMassBMR)
+plot(lmMassBMR)
+plot(fitted(lmMassBMR), res)
+abline(0,0)
+qqnorm(res)
+qqline(res)
+shapiro.test(rstandard(lmMassBMR))
+
+df_short_pos = df_short2[df_short2$GhAhSkew >= 0,]
+df_short_pos$log10GhAhSkew = log10(df_short_pos$GhAhSkew)
+lmMassBMR1 = lm(log10GhAhSkew~log(BMR_value) + log(Mass), data=df_short_pos)
+lmMassBMR12 = lm(GhAhSkew~log(BMR_value) + log(Mass), data=df_short_pos)
+summary(lmMassBMR1)
+summary(lmMassBMR12)
+AIC(lmMassBMR12, lmMassBMR1)
+
+
+
+for (i in unique(df_short2$Orders))
+{
+  a = df_short2[df_short2$Orders == i,]
+  linearMod <- lm(GhAhSkew~log10BMR + log10Mass, data=a)
+  modelSummary = summary(linearMod)
+  c = modelSummary$coefficients[,4][2]
+  b = ggplot(a, aes(x = log10BMR, y = GhAhSkew))+
+    geom_point()+
+    ggtitle(i)+
+    geom_smooth(method = 'lm', formula = y~x)+                            
+    annotate('text', x = (min(a$log10BMR)+1), y = min(a$GhAhSkew), label = round(c, digits = 4))
+  assign(paste0("Order_", i), b)
+  counter = counter + 1
+}
+pdf('Birds_BMR.pdf',         
+    width = 30,height = 100)
+ggarrange(Order_Accipitriformes, Order_Anseriformes, Order_Apodiformes, Order_Apterygiformes, Order_Casuariiformes, Order_Charadriiformes, Order_Coliiformes, Order_Columbiformes, Order_Coraciiformes, Order_Cuculiformes, Order_Falconiformes, Order_Galliformes, Order_Gruiformes, Order_Passeriformes, Order_Pelecaniformes, Order_Piciformes, Order_Procellariiformes, Order_Psittaciformes, Order_Sphenisciformes, Order_Strigiformes, Order_Struthioniformes, Order_Tinamiformes, Order_Upupiformes, 
+          ncol = 3, nrow = 11)
+dev.off()
+
 
 
 
