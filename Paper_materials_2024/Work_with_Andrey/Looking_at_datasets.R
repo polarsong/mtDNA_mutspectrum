5a0fde1c8039f40106e95ebda6551275628a701e
YuryGusarov <yurgusguss@mail.ru>
2024-02-01 13:50:46 +0200

    BMR work

--- Paper_materials_2024/Work_with_Andrey/Looking_at_datasets.R
+++ Paper_materials_2024/Work_with_Andrey/Looking_at_datasets.R
@@ -12,6 +12,11 @@ library(dplyr)
 df11 = read.csv ('Species_life-histories.csv')
 df12 = read.csv('GlobalBMRbase.csv')
 df_mtdna = read.csv('../../Paper_materials_2024/Birds_dataset_paper.csv')
+df_mtdna_orders = unique(df_mtdna[,c(2,5)])
+df_mtdna_orders$taxonomy = sub('formes.*', '', df_mtdna_orders$taxonomy)
+df_mtdna_orders$taxonomy = sub('.*\\ ', '', df_mtdna_orders$taxonomy)
+df_mtdna_orders$taxonomy = paste(df_mtdna_orders$taxonomy, 'formes', sep = '')
+names(df_mtdna_orders) = c('Species', 'Orders')
 names_v = unique(df_mtdna$species_name)
 df_short = data.frame()
 for (i in names_v)
@@ -24,6 +29,58 @@ for (i in names_v)
 }
 names(df_short) = c('Species', 'GhAhSkew', 'ThChSkew')
 df_short$GhAhSkew = as.numeric(df_short$GhAhSkew)
+
+df12_bmr = df12[df12$Trait == 'BMR',]
+names_v = unique(df12_bmr$Original_Name)
+df_short1 = data.frame()
+for (i in names_v)
+{
+  df1 = df12_bmr[df12_bmr$Original_Name == i,]
+  a1 = sum(df1$TraitValue)
+  a2 = nrow(df1)
+  a = a1/a2
+  b = 'BMR'
+  ab = c(i, b, a)
+  df_short1 = rbind(df_short1, ab)
+}
+names(df_short1) = c('Species', 'Trait', 'BMR_value')
+df_short$GhAhSkew = as.numeric(df_short$GhAhSkew)
+
+df_short2 = merge(df_short, df_short1)
+df_short2$BMR_value = as.numeric(df_short2$BMR_value)
+df_short2 = merge(df_short2, df_mtdna_orders)
+lm1 = lm(log10(BMR_value)~GhAhSkew, data = df_short2)
+abc = summary(lm1)
+abc1 = abc$coefficients[,4][2]
+ggplot(df_short2, aes(x = log10(BMR_value), y = GhAhSkew))+
+  geom_point()+
+  geom_smooth(method = 'lm', formula = y~x)+                            
+  annotate('text', x = (min(log10(df_short2$BMR_value))+1), y = min(df_short2$GhAhSkew), label = round(abc1, digits = 4))
+df_short2$log10BMR = log10(df_short2$BMR_value)
+
+counter = 0
+for (i in unique(df_short2$Orders))
+{
+  a = df_short2[df_short2$Orders == i,]
+  linearMod <- lm(log10BMR~GhAhSkew, data=a)
+  modelSummary = summary(linearMod)
+  c = modelSummary$coefficients[,4][2]
+  b = ggplot(a, aes(x = log10BMR, y = GhAhSkew))+
+    geom_point()+
+    ggtitle(i)+
+    geom_smooth(method = 'lm', formula = y~x)+                            
+    annotate('text', x = (min(a$log10BMR)+1), y = min(a$GhAhSkew), label = round(c, digits = 4))
+  assign(paste0("Order_", i), b)
+  counter = counter + 1
+}
+pdf('Birds_BMR.pdf',         
+    width = 30,height = 100)
+ggarrange(Order_Accipitriformes, Order_Anseriformes, Order_Apodiformes, Order_Apterygiformes, Order_Casuariiformes, Order_Charadriiformes, Order_Coliiformes, Order_Columbiformes, Order_Coraciiformes, Order_Cuculiformes, Order_Falconiformes, Order_Galliformes, Order_Gruiformes, Order_Passeriformes, Order_Pelecaniformes, Order_Piciformes, Order_Procellariiformes, Order_Psittaciformes, Order_Sphenisciformes, Order_Strigiformes, Order_Struthioniformes, Order_Tinamiformes, Order_Upupiformes, 
+          ncol = 3, nrow = 11)
+dev.off()
+
+
+
 df_mtdna11 = merge(df_short, df11, by = 'Species')
 ggplot(df_mtdna11, aes(x = Migration, y = GhAhSkew))+
   geom_boxplot()
