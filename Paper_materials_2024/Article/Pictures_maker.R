81e736254cf3d73e23c81f857ae91451a69b0e90
YuryGusarov <yurgusguss@mail.ru>
2024-10-16 16:44:59 +0200

    More paper

--- Paper_materials_2024/Article/Pictures_maker.R
+++ Paper_materials_2024/Article/Pictures_maker.R
@@ -10,8 +10,8 @@ library(ggtree)
 library(stringr)
 library(dplyr)
 #preparing datasets
-df_mtdna = read.csv('Birds_dataset_paper.csv')
-df_nd6 = read.csv('Birds_mtDNA_data.csv')
+df_mtdna = read.csv('../Work_with_Andrey/Birds_dataset_paper.csv', header = TRUE, sep = ';')
+df_nd6 = read.csv('../Birds_mtDNA_data.csv')
 df_nd6$GhAhSkew = (df_nd6$neutral_g - df_nd6$neutral_A)/(df_nd6$neutral_g + df_nd6$neutral_A)
 df_nd6$ThChSkew = (df_nd6$neutral_T - df_nd6$neutral_c)/(df_nd6$neutral_T + df_nd6$neutral_c)
 df_nd6$fTn = df_nd6$neutral_T/df_nd6$neutral_amount
@@ -176,3 +176,39 @@ graph8 = ggplot(new_big, aes(x = gene_name, y = ThChSkew, fill = Class))+
 
 ggarrange(graph1, graph4, graph2, graph3, graph5, graph6, graph7, graph8,
           ncol = 2, nrow = 4)
+
+#TBSS statistics
+col1 = df_nd6[df_nd6$gene_name == 'COX1',]
+col2 = df_nd6[df_nd6$gene_name == 'CYTB',]
+wilcox.test(col1$GhAhSkew,col2$GhAhSkew)
+wilcox.test(col1$ThChSkew, col2$ThChSkew)
+
+col1 = df_nd6[df_nd6$gene_name == 'ND1',]
+col2 = df_nd6[df_nd6$gene_name == 'ND2',]
+wilcox.test(col1$GhAhSkew,col2$GhAhSkew)
+wilcox.test(col1$ThChSkew, col2$ThChSkew)
+
+df_mtdna$ghahSkew = gsub(',', '.', df_mtdna$ghahSkew)
+df_mtdna$chthSkew = gsub(',', '.', df_mtdna$chthSkew)
+df_mtdna$ghahSkew = as.numeric(as.character(df_mtdna$ghahSkew))
+df_mtdna$chthSkew = as.numeric(as.character(df_mtdna$chthSkew))
+df_mtdna_cut = df_mtdna[df_mtdna$gene_name != 'ND1',]
+df_mtdna_cut = df_mtdna_cut[df_mtdna_cut$gene_name != 'ND2',]
+df_mtdna_cut[df_mtdna_cut$Species == "Drepanis coccinea",]$Species = "Vestiaria coccinea"  
+b_names = unique(df_mtdna_cut$Species)
+spearman_rhos_ghahskew = data.frame()
+spearman_rhos_thchskew = data.frame()
+tbss = c(1,2,3,4,5,6,7,8,9,10)
+for (i in b_names)
+{
+  df_bird = df_mtdna_cut[df_mtdna_cut$Species == i,]
+  speart = cor.test(df_bird$ghahSkew, tbss)
+  spearman_rhos_ghahskew = rbind(spearman_rhos, c(i, speart$p.value))
+}
+
+for (i in b_names)
+{
+  df_bird = df_mtdna_cut[df_mtdna_cut$Species == i,]
+  speart = cor.test(df_bird$chthSkew, tbss)
+  spearman_rhos_thchskew = rbind(spearman_rhos, c(i, speart$p.value))
+}
