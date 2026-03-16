9e2f9683ae8c36c017a058c35dd46a7444fd4254
YuryGusarov <yurgusguss@mail.ru>
2026-03-16 22:29:03 +0200

    Figure1

---
+++ Paper_materials_2024/Article/Figure1.R
@@ -0,0 +1,97 @@
+rm(list = ls(all=TRUE))
+library(ggplot2)
+library(ggpubr)
+df_mtdna = read.csv('../Work_with_Andrey/Birds_dataset_paper.csv', header = TRUE, sep = ';')
+df_nd6 = read.csv('../Birds_mtDNA_data.csv')
+df_nd6$GhAhSkew = (df_nd6$neutral_g - df_nd6$neutral_A)/(df_nd6$neutral_g + df_nd6$neutral_A)
+df_nd6$ThChSkew = (df_nd6$neutral_T - df_nd6$neutral_c)/(df_nd6$neutral_T + df_nd6$neutral_c)
+df_nd6$fTn = df_nd6$neutral_T/df_nd6$neutral_amount
+df_nd6$fAn = df_nd6$neutral_A/df_nd6$neutral_amount
+df_nd6$fCn = df_nd6$neutral_c/df_nd6$neutral_amount
+df_nd6$fGn = df_nd6$neutral_g/df_nd6$neutral_amount
+
+df_nd6$GhAhSkew = (df_nd6$neutral_c- df_nd6$neutral_T)/(df_nd6$neutral_c + df_nd6$neutral_T)
+df_nd6$ThChSkew = (df_nd6$neutral_A - df_nd6$neutral_g)/(df_nd6$neutral_A + df_nd6$neutral_g)
+df_nd6$fTn = df_nd6$neutral_A/df_nd6$neutral_amount
+df_nd6$fAn = df_nd6$neutral_T/df_nd6$neutral_amount
+df_nd6$fCn = df_nd6$neutral_g/df_nd6$neutral_amount
+df_nd6$fGn = df_nd6$neutral_c/df_nd6$neutral_amount
+
+SynNuc = read.table('AllGenesCodonUsageNoOverlap.txt', header = TRUE, sep = '\t')
+SynNuc$ghahSkew = ((SynNuc$NeutralC - SynNuc$NeutralT))/((SynNuc$NeutralC + SynNuc$NeutralT))
+SynNuc$chthSkew = ((SynNuc$NeutralA - SynNuc$NeutralG))/((SynNuc$NeutralA + SynNuc$NeutralG))
+new_mam = SynNuc[, c(1, 2, 79, 80)]
+new_mam$Сlass = 'Mammalia'
+new_bird = df_nd6[, c('species_name', 'gene_name', 'GhAhSkew','ThChSkew')]
+new_bird$Сlass = 'Aves'
+new_bird$species_name = gsub(' ', '_', new_bird$species_name)
+new_mam$Gene[new_mam$Gene == 'CytB'] = 'CYTB'
+names(new_mam) = c('species_name', 'gene_name', 'GhAhSkew', 'ThChSkew', 'Class')
+names(new_bird) = c('species_name', 'gene_name', 'GhAhSkew', 'ThChSkew', 'Class')
+
+new_big = rbind(new_mam, new_bird)
+graph1 = ggplot(new_big, aes(x = gene_name, y = GhAhSkew, fill = Class))+
+  geom_boxplot(notch = TRUE, outlier.alpha = FALSE)+
+  xlab('Mitochondrial genes')+
+  ylab('GhAhSkew')+
+  xlim(c("COX1","COX2","ATP8","ATP6","COX3", "ND3", "ND4L","ND4","ND5",'CYTB',"ND6","ND1","ND2"))+
+  ylim(-1,1)+
+  annotate('text', x = 4.5, y = -0.75, label = 'N birds = 766')+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+        legend.position = "none")
+
+graph2 = ggplot(new_big, aes(x = gene_name, y = ThChSkew, fill = Class))+
+  geom_boxplot(notch = TRUE, outlier.alpha = FALSE)+
+  xlab('Mitochondrial genes')+
+  ylab('ThChSkew')+
+  xlim(c("COX1","COX2","ATP8","ATP6","COX3", "ND3", "ND4L","ND4","ND5",'CYTB',"ND6","ND1","ND2"))+
+  ylim(-1,1)+
+  annotate('text', x = 4.5, y = -0.75, label = 'N mammals = 4356')+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+        legend.position = "none")
+
+df_mut = read.csv('MutSpecVertebrates12.csv')
+df_ac = df_mut[df_mut$Mut == 'A>C',]
+df_ac[df_ac$Mut == 'A>C',]$Mut = 'T>G'
+df_ag = df_mut[df_mut$Mut == 'A>G',]
+df_ag[df_ag$Mut == 'A>G',]$Mut = 'T>C'
+df_at = df_mut[df_mut$Mut == 'A>T',]
+df_at[df_at$Mut == 'A>T',]$Mut = 'T>A'
+df_ca = df_mut[df_mut$Mut == 'C>A',]
+df_ca[df_ca$Mut == 'C>A',]$Mut = 'G>T'
+df_cg = df_mut[df_mut$Mut == 'C>G',]
+df_cg[df_cg$Mut == 'C>G',]$Mut = 'G>C'
+df_ct = df_mut[df_mut$Mut == 'C>T',]
+df_ct[df_ct$Mut == 'C>T',]$Mut = 'G>A'
+df_ga = df_mut[df_mut$Mut == 'G>A',]
+df_ga[df_ga$Mut == 'G>A',]$Mut = 'C>T'
+df_gc = df_mut[df_mut$Mut == 'G>C',]
+df_gc[df_gc$Mut == 'G>C',]$Mut = 'C>G'
+df_gt = df_mut[df_mut$Mut == 'G>T',]
+df_gt[df_gt$Mut == 'G>T',]$Mut = 'C>A'
+df_ta = df_mut[df_mut$Mut == 'T>A',]
+df_ta[df_ta$Mut == 'T>A',]$Mut = 'A>T'
+df_tc = df_mut[df_mut$Mut == 'T>C',]
+df_tc[df_tc$Mut == 'T>C',]$Mut = 'A>G'
+df_tg = df_mut[df_mut$Mut == 'T>G',]
+df_tg[df_tg$Mut == 'T>G',]$Mut = 'A>C'
+
+df_mut_cor = rbind(df_ac, df_ag, df_at, df_ca, df_cg, df_ct, df_ga, df_gc, df_gt, df_ta, df_tc, df_ag)
+
+df_mut_aves = df_mut_cor[df_mut_cor$Class == 'Aves',]
+
+df_cytb = df_mut_aves[df_mut_aves$Gene == 'Cytb',]
+df_cytb$color = 'blue'
+df_cytb[df_cytb$Mut =='C>G' | df_cytb$Mut == 'G>C',]$color = 'black'
+df_cytb[df_cytb$Mut =='C>T' | df_cytb$Mut == 'G>A',]$color = 'red'
+df_cytb[df_cytb$Mut =='T>A' | df_cytb$Mut == 'A>T',]$color = 'grey'
+df_cytb[df_cytb$Mut =='T>C' | df_cytb$Mut == 'A>G',]$color = 'green'
+df_cytb[df_cytb$Mut =='T>G' | df_cytb$Mut == 'A>C',]$color = 'pink'
+graph3 = ggplot(df_cytb, aes(x = Mut, y = MutSpec))+
+  geom_bar(stat = 'identity')+
+  xlim(c("C>A","G>T","C>G","G>C","C>T", "G>A", "T>A","A>T","T>C",'A>G',"T>G","A>C"))+
+  ylab('Mutspec for CytB')
+graph3
+graph_final = ggarrange(graph3, graph3, graph1, graph2,
+                     ncol = 2, nrow = 2)
+graph_final
